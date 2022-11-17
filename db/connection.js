9fca7f74627f550a655e4008fb89c7b0e737595b
hlilya <lily.12345@hotmail.co.uk>
2022-11-17 15:21:17 +0000

    updates

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-const config =
-  ENV === "production" ? { connectionString: process.env.DATABASE_URL } : {};
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -11,5 +9,8 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_url not set");
 }
+
+const config =
+  ENV === "production" ? { connectionString: process.env.DATABASE_URL } : {};
 
 module.exports = new Pool(config);
